3c6d281a943df113d9f34f09c1854173fce5732d
BuseYalçın <buseylcin5@gmail.com>
2020-12-27 14:41:07 +0300

    gonderme

---
+++ Kodluyoruz/ders1/Controllers/deneme.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ders1.Controllers
+{
+    public class deneme : Controller
+    {
+        public IActionResult index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Kodluyoruz/ders1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ders1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
